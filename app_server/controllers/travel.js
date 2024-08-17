9129bbd1e8d39b1f8abfb8139c47268056694ad2
bill chipman <wrchipman@gmail.com>
2024-08-17 14:57:37 -0300

    Module 3 completed baseline

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,5 +1,8 @@
+var fs = require("fs");
+var trips = JSON.parse(fs.readFileSync("./data/trips.json", "utf8"));
+
 const travel = (req, res) => {
-  res.render("travel", { title: "Travlr Getaways" });
+  res.render("travel", { title: "Travlr Getaways", trips });
 };
 module.exports = {
   travel,
